06aa26aeabd4251bb6a4002570d6422e71d54ebb
hanoeyf <144418011+hanoeyf@users.noreply.github.com>
2026-02-24 10:51:38 +0700

    Langkah 4 – Optional Catch-All Route

--- Laporan/LAPORAN_Praktikum4.md
+++ Laporan/LAPORAN_Praktikum4.md
@@ -6,3 +6,6 @@ Langkah 2 – Membuat Catch-All Route
 Langkah 3 – Pengujian Catch-All Route 
     ![modifikasi](image-8.png)
     Berapapun banyaknya seqment tetap terbaca 
+Langkah 4 – Optional Catch-All Route
+    ![Rename file slug](image-9.png)
+    Halaman dapat diakses meskipun tanpa parameter. 
--- Praktikum_4/my-app/src/pages/shop/[[...slug]].tsx
+++ Praktikum_4/my-app/src/pages/shop/[[...slug]].tsx
@@ -7,10 +7,11 @@ const halamanToko = () => {
     return (
         <div>   
             <h1>Halaman Toko</h1>
-                  {/* <p>Toko: {`${query.slug && query.slug[0]+"-"+ query.slug[1]}`}</p> */}
-            <p>
-                Toko: {Array.isArray(query.slug) ? query.slug.join("-") : query.slug}
-            </p>
+                <p>Toko: {`${query.slug && query.slug[0]+"-"+ query.slug[1]}`}</p>
+                {/* <p>
+                    Toko: {Array.isArray(query.slug) ? query.slug.join("-") : query.slug}
+                </p> */}
+        
         </div>
     );
 };
