c68f9197cd5796b1c2a268f08db72892f2cd1313
hanoeyf <144418011+hanoeyf@users.noreply.github.com>
2026-03-05 10:39:56 +0700

    PRAKTIKUM 9

---
+++ Laporan/LAPORAN_Praktikum9.md
@@ -0,0 +1,30 @@
+Langkah Praktikum
+Bagian 1 – Setup Halaman SSR
+    Modifikasi file server.tsx :
+    ![Kode](image.png)
+    Jalankan browser
+Bagian 2 – Implementasi getServerSideProps pada server.tsx
+    Jalankan browser
+Bagian 3 – Refactor Type ( produk type )
+    Modifikasi Product.type.ts
+    Modifikasi file server.tsx 
+Bagian 4 – Uji Perbedaan SSR vs CSR
+
+Tugas Praktikum
+Tugas Individu
+1. Buat 2 halaman:
+    o /products (CSR)
+    o /products/server (SSR)
+2. Dokumentasikan:
+    o Screenshot CSR
+    o Screenshot SSR
+    o Perbedaan Network tab
+    o Perbedaan View Source
+3. Buat laporan analisis minimal 2 halaman.
+
+E. Studi Analisis
+Jawab pertanyaan berikut:
+1. Mengapa SSR lebih baik untuk SEO?
+2. Kapan sebaiknya menggunakan SSR?
+3. Apa kekurangan SSR dibanding CSR?
+4. Mengapa skeleton tidak muncul pada SSR?
---
+++ Praktikum_9/my-app/src/pages/produk/index.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import TampilanProduk from "../../views/product";
+import useSWR from "swr";
+import fetcher from "../utlis/swr/fetcher";
+
+// const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+const kategori = () => {
+  const [products, setProducts] = useState([]);
+  const { data, error, isLoading } = useSWR("/api/produk", fetcher);
+  // const [loading, setLoading] = useState(false);
+  // const fetchProducts = async () => {
+  //   try {
+  //     setLoading(true);
+  //     const response = await fetch("/api/produk");
+  //     const responsedata = await response.json();
+  //     setProducts(responsedata.data);
+  //   } catch (error) {
+  //     console.error("Error fetching products:", error);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
+  return (
+    <div>
+      <TampilanProduk products={isLoading ? [] : data.data} />
+    </div>
+  );
+};
+
+export default kategori;
