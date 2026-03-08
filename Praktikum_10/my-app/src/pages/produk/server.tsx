3c88962de8372135d330c422c321151e6ac06b6f
hanoeyf <144418011+hanoeyf@users.noreply.github.com>
2026-03-08 16:57:18 +0700

    PRAKTIKUM 10

---
+++ Praktikum_10/my-app/src/pages/produk/server.tsx
@@ -0,0 +1,23 @@
+import TampilanProduk from "../../views/product";
+import { ProductType } from "../types/Product.type";
+
+const halamanProdukServer = (props: { products: ProductType[] }) => {
+  const { products } = props;
+  return (
+    <div>
+      <h1>Halaman Produk Server</h1>
+      <TampilanProduk products={products} />
+    </div>
+  );
+};
+  export default halamanProdukServer;
+
+  export async function getServerSideProps() {
+    const res = await fetch("http://localhost:3000/api/produk");
+    const respone = await res.json();
+    return {
+      props: {
+        products: respone.data,
+      },
+    };
+  }
