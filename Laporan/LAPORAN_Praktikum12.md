bdad9d1f05d7b00245d189fef661878796cedcd7
hanoeyf <144418011+hanoeyf@users.noreply.github.com>
2026-03-12 09:26:47 +0700

    PRAKTIKUM 12

---
+++ Laporan/LAPORAN_Praktikum12.md
@@ -0,0 +1,31 @@
+=> Implementasi ISR Otomatis
+    Bagian 1 – Tambahkan revalidate
+    Bagian 2 – Pengujian ISR
+
+=> On-Demand Revalidation
+    Bagian 1 – Buat API Revalidate
+    Bagian 2 – Tambahkan Parameter Data
+        Modifikasi file revalidate.ts
+    Bagian 3 – Tambahkan Token Security
+        Modifikasi file revalidate.ts
+
+=> Pengujian Manual Revalidation
+
+=> Tugas Praktikum
+    Tugas Individu
+    1. Tambahkan lagi produk pada firebase
+    2. Implementasikan ISR dengan revalidate: 10.
+    3. Tambahkan endpoint On-Demand Revalidation.
+    4. Tambahkan validasi token.
+
+    5. Uji dengan:
+    o Token benar
+    o Token salah
+    o Tanpa token
+
+    H. Pertanyaan Analisis
+    1. Mengapa ISR lebih fleksibel dibanding SSG?
+    2. Apa perbedaan revalidate waktu dan on-demand?
+    3. Mengapa endpoint revalidation harus diamankan?
+    4. Apa risiko jika token tidak digunakan?
+    5. Kapan ISR lebih cocok dibanding SSR?
---
+++ Praktikum_12/my-app/src/pages/produk/[produk].tsx
@@ -0,0 +1,51 @@
+import fetcher from "@/utlis/swr/fetcher";
+import { useRouter } from "next/router";
+import useSWR from "swr";
+import DetailProduct from "../../views/DetailProduct";
+import { ProductType } from "@/types/Product.type";
+
+const HalamanProduk = ({ product }: { product: ProductType }) => {
+    // const {query} = useRouter();
+    // const {data, error, isLoading} = useSWR(`/api/products/${query.produk}`, fetcher);
+    return (
+        <div>
+            <DetailProduct products={product} />
+        </div>
+    );
+};
+
+export default HalamanProduk;
+
+// export async function getServerSideProps ({ params }: { params: { produk: string}}){
+//     const res = await fetch(`http://localhost:3000/api/produk/${params?.produk}`);
+//     const respone = await res.json();
+
+//     return{
+//         props: {
+//             product: respone.data,
+//         },
+//     };
+// }
+
+export async function getStaticPaths() {
+    const res = await fetch(`http://localhost:3000/api/products`);
+    const respone = await res.json()
+
+    const paths = respone.data.map((product: ProductType) => ({
+        params: { produk: product.id }
+    }))
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps({ params }: { params: { produk: string } }) {
+    const res = await fetch(`http://localhost:3000/api/products/${params.produk}`);
+    const respone: { data: ProductType[] } = await res.json();   
+    return {
+        props: {
+            product: respone.data,
+        },
+    };
+}
