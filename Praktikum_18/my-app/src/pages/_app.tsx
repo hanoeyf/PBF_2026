0e245b85a369e364b58911ffe57493aca17451bb
hanoeyf <144418011+hanoeyf@users.noreply.github.com>
2026-04-04 22:18:08 +0700

    PRAKTIKUM18

---
+++ Praktikum_18/my-app/src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import AppShell from '@/components/layouts/Appshell';
+import { useRouter } from 'next/router';
+import { SessionProvider } from 'next-auth/react';
+
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+
+  return (
+    <SessionProvider session={pageProps.session}>
+      <AppShell>
+        <Component {...pageProps} />
+      </AppShell>
+    </SessionProvider>
+  );
+}
---
+++ Praktikum_18/my-app/src/pages/admin/index.tsx
@@ -0,0 +1,16 @@
+const HalamanAdmin = () => {
+  return (
+    <div>
+        <div className="admin">
+            <h1>Halaman Admin</h1>
+            <p>
+                Selamat datang di halaman admin! 
+                Di sini Anda dapat mengelola konten dan pengguna aplikasi Anda.
+            </p>
+
+        </div>
+    </div>
+    );
+};
+
+export default HalamanAdmin;
